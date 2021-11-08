a9443095e9de588f89a9f3fd3cc176eed8352f60
Shahwar Khalid <itisshahwar@gmail.com>
2021-11-09 02:15:02 +0500

    Added root url, heroku site link to readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include Respondable
+
+  def root
+    redirect_to '/api-docs/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
 
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
+
+  root to: 'application#root'
 end
